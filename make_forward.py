b80a417ee3b58cb223b5b38334bdad454fb0f5dd
neural-decoder <tomasz.gorski.14@gmail.com>
2024-12-10 20:38:56 +0100

    minor

--- make_forward.py
+++ make_forward.py
@@ -7,7 +7,34 @@ import os
 import mne
 
 
-def make_forward(subjects_dir, subject, measurement, overwrite=False):
+def make_forward(subjects_dir, subject, measurement, overwrite=False, n_jobs=None):
+
+
+    """
+    Create and save a forward solution for MEG/EEG source analysis.
+
+    The function constructs a forward solution using a provided subject’s anatomical,
+    surface, and measurement data. It reads necessary BEM, source space, info, and
+    transformation files, computes the forward solution, and saves it to the specified path.
+
+    :param subjects_dir: Path to the subjects directory that contains all subject data.
+    :type subjects_dir: str
+
+    :param subject: Name of the subject whose data will be processed.
+    :type subject: str
+
+    :param measurement: Measurement folder that contains the required info data.
+    :type measurement: str
+
+    :param overwrite: Whether to overwrite an existing forward solution file. Default is False.
+    :type overwrite: bool
+
+    :param n_jobs: Number of parallel jobs to use for computation. Default is None.
+    :type n_jobs: int, optional
+
+    :return: None
+    :raises CustomError: Raised at various points if required files or directories are not found.
+    """
 
 
     bem_path = str(os.path.join(subjects_dir, subject, 'bem'))
@@ -44,6 +71,6 @@ def make_forward(subjects_dir, subject, measurement, overwrite=False):
 
     forward_path = os.path.join(subjects_dir, subject, measurement, 'forward', 'forward.fif')
 
-    fwd = mne.make_forward_solution(info, trans=trans, src=src, bem=bem)
+    fwd = mne.make_forward_solution(info=info, trans=trans, src=src, bem=bem, n_jobs=n_jobs)
 
     fwd.save(forward_path, overwrite=overwrite)
